e1eb6bb8a9cbd1e76ce86b2bf79286f9039ea477
Loganathan183 <viratlogu183gmail.com>
2024-08-19 12:02:14 +0530

    dsa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/Defaultlayout";
+import Home from './page/home';
+import Card from './page/Cart';
 
 
 function App() {
   return (
-<div></div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}> </Route>
+        <Route path="" element={<Home />}></Route>
+        <Route path="/Cart" element={<Card />}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,21 @@
-import {container} from "react-botstrap";
-import styles from './header.module.css'
-export default function(){
-    return(
-<div classname={Style.header}>
-    
- <container>
 
- </container>
-</div>
+import { Container } from "react-bootstrap";
+import { FaCartArrowDown } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styles from './header.module.css';
+
+export default function Header() {
+    return (
+        <div classname={styles.header}>
+         <Container>
+                <div style={{color: "red",background_color: "white"}}>
+                    <h1>hii</h1>
+                    <Link to="cart" style={styles.cartIcon}>
+                        <FaCartArrowDown />
+                        <span>1</span>
+                    </Link>
+                </div>
+           </Container>
+        </div>
     )
 };
